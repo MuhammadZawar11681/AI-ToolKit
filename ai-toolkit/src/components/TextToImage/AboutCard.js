8f5b7ae1b58ac199a3d40a5965f6186ac226d956
Yushaa <11997.y.sid@gmail.com>
2023-12-07 13:34:59 +0500

    Committing local changes before pulling

--- ai-toolkit/src/components/TextToImage/AboutCard.js
+++ ai-toolkit/src/components/TextToImage/AboutCard.js
@@ -8,12 +8,12 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-          here u add Text to image CODE
-           
-          </p>
-          
+            here u add Text to image CODE
 
-          
+          </p>
+
+
+
           {/* <footer className="blockquote-footer">NULL</footer> */}
         </blockquote>
       </Card.Body>
