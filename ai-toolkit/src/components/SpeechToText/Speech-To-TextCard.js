0f791955e63b6402d38101ed4de8a10ce6b511e3
Muhammad Zawar <mzawar189@gmail.com>
2024-03-02 09:38:33 +0500

    Update Text To Speech Module

--- ai-toolkit/src/components/SpeechToText/Speech-To-TextCard.js
+++ ai-toolkit/src/components/SpeechToText/Speech-To-TextCard.js
@@ -2,15 +2,60 @@ import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 import useClipboard from "react-use-clipboard";
-import React from "react";
+//import React from "react";
 import Card from "react-bootstrap/Card";
 import "./SpeechToText.css";
+import React, { useState, useRef, useEffect } from "react";
 
 // import SpeechRecognition from 'react-speech-recognition';
+
+import axios from "axios";
+const KEY = process.env.REACT_APP_OPENAI_API;
+const model = "whisper-1";
 
 const SpeechToTextCard = () => {
   const { transcript, resetTranscript } = useSpeechRecognition();
   const [isCopied, setCopied] = useClipboard(transcript);
+
+  const { browserSupportsSpeechRecognition } = useSpeechRecognition();
+
+  const inputRef = useRef();
+  const [file, setFile] = useState();
+  const [response, setResponse] = useState();
+  const onChangeFile = () => {
+    setFile(inputRef.current.files[0]);
+  };
+  useEffect(() => {
+    const fetchAudioFile = async () => {
+      if (!file) {
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append("model", model);
+      formData.append("file", file);
+
+      axios
+        .post("https://api.openai.com/v1/audio/transcriptions ", formData, {
+          headers: {
+            "content-type": "multipart/form-data",
+            Authorization: `Bearer ${KEY}`,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          setResponse(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    fetchAudioFile();
+  }, [file]);
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
 
   return (
     <Card className="quote-card-view h-600">
@@ -51,6 +96,19 @@ const SpeechToTextCard = () => {
               </button>
             </div>
           </div>
+          <div className="file-container">
+            <h1>
+              upload audio file to convert it to speech!
+              <input
+                type="file"
+                ref={inputRef}
+                accept=".mp3"
+                onChange={onChangeFile}
+                style={{ display: "block", marginTop: "20px" }}
+              />
+              {response && <div>{JSON.stringify(response, null, 2)} </div>}
+            </h1>
+          </div>
         </>
       </Card.Body>
     </Card>
@@ -58,3 +116,137 @@ const SpeechToTextCard = () => {
 };
 
 export default SpeechToTextCard;
+
+//yousha code
+
+// import SpeechRecognition, {
+//   useSpeechRecognition,
+// } from "react-speech-recognition";
+// import React, { useState, useRef, useEffect } from "react";
+
+// import useClipboard from "react-use-clipboard";
+
+// import Card from "react-bootstrap/Card";
+// import "./SpeechToText.css";
+// import SpeechRecognition from 'react-speech-recognition';
+// import axios from "axios";
+// const KEY = process.env.REACT_APP_OPENAI_API;
+// const model = "whisper-1"
+
+// const SpeechToTextCard = () => {
+//   // const [textToCopy, setTextToCopy] = useState();
+//   // const [isCopied, setCopied] = useClipboard(textToCopy, {
+//   //   successDuration: 1000,
+//   // });
+
+// const startListening = () =>
+//   SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+// const { transcript, browserSupportsSpeechRecognition } =
+//   useSpeechRecognition();
+
+//   // const inputRef = useRef();
+//   // const [file, setFile] = useState();
+//   // const [response, setResponse] = useState();
+//   // const onChangeFile = () => {
+//   //   setFile(inputRef.current.files[0]);
+//   // }
+//   // useEffect(() => {
+//   //   const fetchAudioFile = async () => {
+//   //     if (!file) {
+//   //       return;
+//   //     }
+
+//   //     const formData = new FormData();
+//   //     formData.append("model", model);
+//   //     formData.append("file", file);
+
+//   //     axios
+//   //       .post("https://api.openai.com/v1/audio/transcriptions ", formData, {
+//   //         headers: {
+//   //           "content-type": "multipart/form-data",
+//   //           Authorization: `Bearer ${KEY}`,
+
+//   //         },
+//   //       })
+//   //       .then((res) => {
+//   //         console.log(res.data);
+//   //         setResponse(res.data)
+//   //       })
+//   //       .catch((err) => {
+//   //         console.log(err);
+//   //       })
+//   //   };
+//   //   fetchAudioFile();
+
+//   // }, [file]);
+
+//   // if (!browserSupportsSpeechRecognition) {
+//   //   return null;
+//   // };
+
+//   return (
+
+//     <div className="parent">
+
+//       <Card className="quote-card-view h-600 main-container" >
+//         <Card.Body>
+//           <blockquote className="blockquote mb-0 text-center">
+//             <div className="container mt-3">
+//               <h1 className="mb-4">
+//                 Convert your Speech To Text With{" "}
+//                 <span className="purple">AI</span>
+//               </h1>
+//             </div>
+//           </blockquote>
+//           <>
+//             <div className="main-container">
+
+//               {/* <div className="speech-container">
+//                 <div
+//                   className="speech-main-content"
+//                   onClick={() => setTextToCopy(transcript)}
+//                 >
+//                   {transcript}
+//                 </div>
+
+//                 <div className="speech-btn-style">
+//                   <button className="speech-button" onClick={setCopied}>
+//                     {isCopied ? "Copied!" : "Copy to clipboard"}
+//                   </button>
+//                   <button className="speech-button" onClick={startListening}>
+//                     Start Listening
+//                   </button>
+//                   <button
+//                     className="speech-button"
+//                     onClick={SpeechRecognition.stopListening}
+//                   >
+//                     Stop Listening
+//                   </button>
+//                 </div>
+//               </div> */}
+//               {/* <div className="file-container">
+//                 <h1>
+//                   upload audio file to convert it to speech!
+
+//                   <input type="file"
+//                     ref={inputRef}
+//                     accept=".mp3"
+//                     onChange={onChangeFile}
+//                     style={{ display: "block", marginTop: "20px" }} />
+
+//                   {response && <div>{JSON.stringify(response, null, 2)} </div>}
+
+//                 </h1>
+//               </div> */}
+
+//             </div>
+//           </>
+//         </Card.Body>
+//       </Card>
+
+//     </div>
+
+//   );
+// };
+
+// export default SpeechToTextCard;
