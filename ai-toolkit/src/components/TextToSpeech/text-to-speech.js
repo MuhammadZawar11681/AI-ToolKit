e0ee435eb0410eb6a4a5b36c30285f23dbfbc5de
Yushaa <11997.y.sid@gmail.com>
2024-03-05 12:35:54 +0500

    for switching to another file i-e login/signup have to commit previous code first

--- ai-toolkit/src/components/TextToSpeech/text-to-speech.js
+++ ai-toolkit/src/components/TextToSpeech/text-to-speech.js
@@ -1,41 +1,239 @@
-import React from "react";
+// import React from "react";
+// import { Container, Row, Col } from "react-bootstrap";
+// import Particle from "../Particle";
+// import TextToSpeechCard from "./Text-to-SpeechCard";
+
+
+// function TextToSpeech() {
+//     return (
+//         <Container fluid className="about-section">
+
+//             <Container>
+//                 <Row style={{ justifyContent: "center", padding: "10px" }}>
+//                     <Col
+//                         md={7}
+//                         style={{
+//                             justifyContent: "center",
+//                             paddingTop: "30px",
+//                             paddingBottom: "50px",
+//                         }}
+//                     >
+//                         <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+//                             <strong className="purple">Text To Speech Conversion</strong>
+//                         </h1>
+//                         <TextToSpeechCard />
+//                     </Col>
+
+//                 </Row>
+
+
+//                 <br />
+//                 <br />
+
+
+
+//             </Container>
+//             <Particle />
+//         </Container>
+//     );
+// }
+
+// export default TextToSpeech;
+
+import { React } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import TextToSpeechCard from "./Text-to-SpeechCard";
 
 
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import SOUND_MODELS from "./constants";
+import { Form, Button, FormGroup, FormLabel, FormControl, FormText } from 'react-bootstrap';
+import Loader from "./Loader";
+import { useState } from "react";
+import './textToSpeech.css';
+
+
+const FormSchema = z.object({
+    soundModel: z.string({
+        required_error: "Please select a Hugging Face sound model to use.",
+    }),
+    text: z.string({
+        required_error: "Please select a text for the model to use.",
+    }),
+});
+
+
 function TextToSpeech() {
+
+    // State to manage loading status and audio URL
+    const [isLoading, setIsLoading] = useState(false);
+    const [audioUrl, setAudioUrl] = useState(null);
+    // State for tracking form submission status
+    const [formSubmitting, setFormSubmitting] = useState(false);
+
+    // Initialize the react-hook-form with the validation schema
+    const { register, handleSubmit } = useForm();
+
+    const handleGetAudio = async (formData) => {
+        setIsLoading(true);
+
+        try {
+            // Make a POST request to the server's API endpoint to generate audio
+            // Make a POST request to the specified 'modelUrl' using Hugging Face token for authorization
+            const response = await fetch(
+                formData.modelUrl,
+                {
+                    headers: {
+                        Authorization: `Bearer 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 `,
+                        // Authorization: `Bearer  ${process.env.HUGGING_FACE_TOKEN}`, // Use the correct token
+                        "Content-Type": "application/json",
+                    },
+                    method: "POST",
+                    body: JSON.stringify({ inputs: formData.text }),
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch audio data.");
+            }
+
+            // Get the audio data as an ArrayBuffer
+            const data = await response.arrayBuffer();
+
+            // Convert ArrayBuffer to Blob and create a URL for the audio
+            const blob = new Blob([data], { type: "audio/flac" });
+            const audioUrl = URL.createObjectURL(blob);
+            setAudioUrl(audioUrl);
+            setIsLoading(false);
+        } catch (error) {
+            setIsLoading(false);
+        }
+    };
+
+    // Function to handle form submission
+    function onSubmit(data) {
+
+        console.log(data)
+        // FormSchema.parse(data)
+        setFormSubmitting(true);
+
+        // Prepare the sound request object
+        const soundRequest = {
+            modelUrl: data.soundModel,
+            text: data.text,
+        };
+
+        // Call the provided handler function with the sound request
+        handleGetAudio(soundRequest);
+
+        setFormSubmitting(false);
+    }
+
+
+
+
     return (
-        <Container fluid className="about-section">
-
-            <Container>
-                <Row style={{ justifyContent: "center", padding: "10px" }}>
-                    <Col
-                        md={7}
-                        style={{
-                            justifyContent: "center",
-                            paddingTop: "30px",
-                            paddingBottom: "50px",
-                        }}
-                    >
-                        <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-                            <strong className="purple">Text To Speech Conversion</strong>
-                        </h1>
-                        <TextToSpeechCard />
-                    </Col>
-
-                </Row>
 
 
-                <br />
-                <br />
+        <>
+            <div>
+                <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+                    <strong className="purple">Text To Speech Conversion</strong>
+                </h1>
+            </div>
+
+            <div className="main-container d-flex flex-column flex-md-row h-100">
+
+                <div className="w-100 w-md-1/3 p-4">
+                    <div className="ms-4 me-4 mt-4 mb-4 text-xl">
+                        <h1>Text to Speech</h1>
+                    </div>
+                    <div className="ms-4 me-4">
+                        {/* Form component that uses react-hook-form */}
+                        <Form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+                            {/* Form field for selecting the sound model */}
+                            <Form.Group>
+                                <Form.Label>Sound Model</Form.Label>
+                                {/* Select component for choosing a sound model */}
 
 
+                                {/* <Form.Control
+                                as="select"
+                                // onChange={form.handleChange}
+                                // value={form.values.soundModel}
+                                disabled={formSubmitting}
+                            >
+                                <Form.Select>
+                                    {/* Map through available sound models */}
+                                {/* {SOUND_MODELS.map((model, index) => (
+                                        <option key={`${model.name}-${index}`} value={model.url}>
+                                            {model.name}
+                                        </option> */}
+                                {/* ))} */}
+                                {/* </Form.Select> } */}
 
-            </Container>
-            <Particle />
 
-        </Container>
+                                {/* NEW BOOT STRAP CODE HERE */}
+
+
+                                {/* </Form.Control>  */}
+
+                                {/* ADDED NEW BOOTSTRAP CODE HERE ON 15/2/24 ON 2:56PM */}
+                                <select className="form-select" aria-label="Default select example" {...register("soundModel")}>
+
+                                    {SOUND_MODELS.map((model, index) => (
+                                        <option key={`${model.name}-${index}`} value={model.url}>
+                                            {model.name}
+                                        </option>))}
+                                </select>
+
+                                <Form.Text className="text-muted">This model will generate your sound.</Form.Text>
+                            </Form.Group>
+                            {/* Form field for entering the text */}
+                            <Form.Group>
+                                <Form.Label>Text</Form.Label>
+                                <Form.Control
+                                    as="textarea"
+                                    rows={6}
+                                    placeholder="Enter your text here..."
+                                    name="text"
+                                    // value={form.values.text}
+                                    // onChange={form.handleChange}
+                                    disabled={formSubmitting}
+                                    {...register("text")}
+                                />
+                                <Form.Text className="text-muted">The text used to convert to sound.</Form.Text>
+                            </Form.Group>
+                            {/* Submit button */}
+                            <Button type="submit" disabled={formSubmitting}>
+                                Submit
+                            </Button>
+                        </Form>
+                    </div>
+                </div>
+                <div className="gen-sound w-100 w-md-2/3 p-4 bg-light h-100">
+                    <div className="gen-sound2 h-100 d-flex justify-content-center align-items-center">
+                        {isLoading ? (
+                            // Show loader when fetching audio data
+                            <Loader />
+                        ) : (
+                            // Display audio player when audio is available
+                            <>
+                                {audioUrl && (
+                                    <audio controls>
+                                        <source id="audioSource" type="audio/flac" src={audioUrl} />
+                                    </audio>
+                                )}
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div >
+        </>
+
     );
 }
 
