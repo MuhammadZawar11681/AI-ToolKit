3934e24abc9a18df73267c66d79d0941cba38501
Muhammad Zawar <mzawar189@gmail.com>
2024-03-22 14:33:47 +0500

    Mini changes in Speech to text module

--- ai-toolkit/src/components/SpeechToText/speech-to-text.js
+++ ai-toolkit/src/components/SpeechToText/speech-to-text.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import Particle from "../Particle";
+//import Particle from "../Particle";
 // import TextToSpeechCard from "./Text-to-SpeechCard";
 import SpeechToTextCard from "./Speech-To-TextCard";
 
