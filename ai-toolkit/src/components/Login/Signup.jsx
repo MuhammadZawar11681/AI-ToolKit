66e31247d12bdd79fe9de19fd2d330c19a2c5ce6
Muhammad Zawar <mzawar189@gmail.com>
2024-04-24 18:16:35 +0500

    Remove Unuse variables

--- ai-toolkit/src/components/Login/Signup.jsx
+++ ai-toolkit/src/components/Login/Signup.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link, Navigate } from "react-router-dom";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
