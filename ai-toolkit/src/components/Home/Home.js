6355ea74c6c0605dee70b2b0199e15711092cfa1
Muhammad Zawar <mzawar189@gmail.com>
2024-06-21 17:29:54 +0500

    Change Signin/Signup Background

--- ai-toolkit/src/components/Home/Home.js
+++ ai-toolkit/src/components/Home/Home.js
@@ -38,12 +38,12 @@ function Home() {
                 className="img-fluid"
                 style={{ maxHeight: "450px" }}
               /> */}
-               <ImageSlider />
+              <ImageSlider />
             </Col>
           </Row>
         </Container>
       </Container>
-     
+
       <Home2 />
       <HomeFooter />
     </section>
