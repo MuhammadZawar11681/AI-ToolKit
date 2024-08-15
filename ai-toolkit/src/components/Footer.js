ee6568493c811024a7ee5e8c323308507aafdcb7
Muhammad Zawar <mzawar189@gmail.com>
2024-08-15 11:08:14 +0500

    Add Routers And links in footer.

--- ai-toolkit/src/components/Footer.js
+++ ai-toolkit/src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href=" "
+                href="https://github.com/MuhammadZawar11681https://github.com/MuhammadZawar11681/AI-ToolKit.git"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href=" "
+                href="https://www.linkedin.com/in/m-zawar/"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href=" "
+                href="https://www.instagram.com/zawar.mohammad/"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
