c72364890374035d9904627e78d305fe5cd8d1b5
Muhammad Zawar <mzawar189@gmail.com>
2024-09-04 11:40:50 +0500

    Change Login Promp & Resize Logo and add route

--- ai-toolkit/src/components/LoginPrompt/LoginPrompt1.js
+++ ai-toolkit/src/components/LoginPrompt/LoginPrompt1.js
@@ -1,50 +1,3 @@
-// import React from "react";
-// import { Container, Row, Col } from "react-bootstrap";
-// import Particle from "../Particle";
-// import LoginPrompt2 from "./LoginPrompt2";
-
-// function LoginPromp1() {
-//   return (
-//     <Container
-//       fluid
-//       className="about-section bg-gradient-to-r from-gray-800 to-gray-900 py-5"
-//     >
-//       <Particle />
-//       <Container>
-//         <Row className="justify-content-center text-center">
-//           <Col
-//             md={7}
-//             className="animate__animated animate__fadeInUp"
-//             style={{
-//               paddingTop: "30px",
-//               paddingBottom: "50px",
-//             }}
-//           >
-//             <h1 className="display-4 text-white font-bold">
-//               <strong className="text-purple-500">
-//               Access Denied
-//               </strong>
-//             </h1>
-//             <h2 className="text-lg text-white mt-4">
-//             Please sign in to access{" "}
-//               <span className="text-purple-500">this tool.</span>
-//             </h2>
-//           </Col>
-//         </Row>
-
-//         <Row className="justify-content-center">
-//           <Col md={8}>
-//             <LoginPrompt2 />
-//           </Col>
-//         </Row>
-//       </Container>
-//     </Container>
-//   );
-// }
-
-// export default LoginPromp1;
-
-
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
@@ -52,28 +5,17 @@ import LoginPrompt2 from "./LoginPrompt2";
 
 function LoginPromp1() {
   return (
-    <Container fluid className="bg-dark py-5">
-      
+    <Container fluid className="py-5">
       <Container>
         <Row className="justify-content-center text-center">
           <Col
             md={7}
-            className="animate__animated animate__fadeInUp"
+            className="animate__animated animate__slideInDown"
             style={{
-              paddingTop: "30px",
+              paddingTop: "90px",
               paddingBottom: "50px",
             }}
-          >
-            <h1 className="display-4 text-white font-weight-bold">
-              <strong className="text-primary">
-                Access Denied
-              </strong>
-            </h1>
-            <h2 className="text-white mt-4">
-              Please sign in to access{" "}
-              <span className="text-primary">this tool.</span>
-            </h2>
-          </Col>
+          ></Col>
         </Row>
 
         <Row className="justify-content-center">
