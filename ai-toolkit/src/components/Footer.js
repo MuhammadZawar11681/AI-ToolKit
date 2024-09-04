a6dd3016cad6fd292e263ab677090bfb1cf61fc0
Muhammad Zawar <mzawar189@gmail.com>
2024-09-04 15:21:19 +0500

    Add git hub link

--- ai-toolkit/src/components/Footer.js
+++ ai-toolkit/src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/MuhammadZawar11681https://github.com/MuhammadZawar11681/AI-ToolKit.git"
+                href="https://github.com/MuhammadZawar11681/AI-ToolKit.git"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
